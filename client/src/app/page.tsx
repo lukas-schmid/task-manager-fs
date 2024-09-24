263ef28901df1f81ffae6394e3fac7c2811819bc
lukas-schmid <schmid.luk@hotmail.com>
2024-09-24 10:34:09 +0200

    fix build warnings and error

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -22,11 +22,11 @@ export default function Home() {
       <main className="mx-auto w-full flex flex-col items-center p-2 max-w-5xl gap-16">
         <div className="flex flex-col lg:flex-row items-center justify-center gap-10">
           <div className="max-w-xl flex flex-col gap-3">
-            <h2 className="text-2xl text-gradient-blue font-extrabold">
+            <h2 className="text-3xl text-gradient-blue font-extrabold">
               Our task management app empowers teams to collaborate more
               effectively and enhance productivity
             </h2>
-            <p>
+            <p className="text-xl">
               With intuitive boards, lists, and cards, teams can effortlessly
               organize and prioritize projects in an engaging, adaptable, and
               satisfying manner.
@@ -37,21 +37,21 @@ export default function Home() {
         <div className="flex lg:flex-row flex-col-reverse items-center justify-center gap-10">
           <Image src={team} alt="Hero image with tasks and people" />
           <div className="max-w-xl flex flex-col gap-3">
-            <h2 className="text-2xl text-gradient-blue font-extrabold">
+            <h2 className="text-3xl text-gradient-blue font-extrabold">
               Collaborate with Any Team
             </h2>
-            <p>
-              Whether it's for work, a side project, or planning your next
+            <p className="text-xl">
+              Whether it&apos;s for work, a side project, or planning your next
               family vacation, our app keeps your team organized and on track.
             </p>
           </div>
         </div>
         <div className="flex flex-col lg:flex-row items-center justify-center gap-10">
           <div className="max-w-xl flex flex-col gap-3">
-            <h2 className="text-2xl text-gradient-blue font-extrabold">
+            <h2 className="text-3xl text-gradient-blue font-extrabold">
               Key Insights at a Glance
             </h2>
-            <p>
+            <p className="text-xl">
               Explore the details by adding comments, attachments, due dates,
               and more directly to your task cards. Collaborate on projects from
               start to finish.
@@ -64,15 +64,15 @@ export default function Home() {
             <Image src={workflow} alt="Hero image with tasks and people" />
           </div>
           <div className="max-w-xl flex flex-col gap-3">
-            <h2 className="text-2xl text-gradient-blue font-extrabold">
+            <h2 className="text-3xl text-gradient-blue font-extrabold">
               Integrated Workflow Automation
             </h2>
-            <p>
+            <p className="text-xl">
               Let automation handle the repetitive tasks! Enhance productivity
               by harnessing the power of automated workflows across your team.
               Eliminate tedious tasks from your to-do lists with:
             </p>
-            <ul>
+            <ul className="list-inside list-disc">
               <li>Rule-Based Triggers</li>
               <li>Custom Card & Board Buttons</li>
               <li>Calendar Commands</li>
